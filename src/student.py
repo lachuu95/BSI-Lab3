39237b8f39421ef2a9c92454f7c6399cfdae19a7
Błażej Łach <lachuu95@gmail.com>
2019-11-26 06:16:12 +0100

    dodanie testów

--- src/student.py
+++ src/student.py
@@ -1,3 +1,44 @@
-class Student():
-    def __init__(self):
-        pass
+class Student:
+    def __init__(self, name: str, surname: str, id: str):
+        self.__name = self.__validate_str(name)
+        self.__surname = self.__validate_str(surname)
+        self.__id = self.__validate_num(id)
+        self.__marks = []
+
+    def __validate_str(self, content: str) -> str:
+        if not content.isalpha():
+            raise ValueError("no xd ja tu chce literki a nie coś innego")
+        if not content[0].isupper():
+            raise ValueError("nazwy powinny zaczynać Wielką literą")
+        return content
+
+    def __validate_num(self, content: str) -> str:
+        if not content.isdigit():
+            raise ValueError("no gdzie mnie tu z literkami chcę cyferki")
+        return content
+
+    @property
+    def name(self) -> str:
+        return self.__name
+    
+    @property
+    def surname(self) -> str:
+        return self.__surname
+
+    @property
+    def id(self) -> str:
+        return self.__id
+
+    @name.setter
+    def name(self, value:str) -> None:
+        self.__name = self.__validate_str(value)
+
+    @surname.setter
+    def surname(self, value:str) -> None:
+        self.__surname = self.__validate_str(value)
+
+    @id.setter
+    def id(self, value:str) -> None:
+        self.__id = self.__validate_num(value)   
+
+
--- test/src/test_student.py
+++ test/src/test_student.py
@@ -1,7 +1,52 @@
 import pytest
 from src.student import Student
 
-def test_student():
-    Student()
-    assert True
+
+@pytest.mark.parametrize(
+    "name, surname, student_id",
+    [
+        ("Qwerty", "Azerty", "000001"),
+        ("Asdfgh", "Zxcvbn", "000002"),
+        ("Qazwsx", "Edcrfv", "000003"),
+    ],
+)
+def test_student_init(name, surname, student_id):
+    student = Student(name, surname, student_id)
+    assert student.name is name
+    assert student.surname is surname
+    assert student.id is student_id
+
+
+@pytest.mark.parametrize(
+    "name, surname, student_id",
+    [
+        ("awerty", "azerty", "000001"),
+        ("213546", ",./;l=", "000002"),
+        ("Qazwsx", "Edcrfv", "/';.][]"),
+    ],
+)
+def test_student_init_wrong(name, surname, student_id):
+    with pytest.raises(ValueError):
+        Student(name, surname, student_id)
+
+@pytest.mark.parametrize(
+    "name, surname, student_id",
+    [
+        ("Qwerty", "Azerty", "000001"),
+        ("Asdfgh", "Zxcvbn", "000002"),
+        ("Qazwsx", "Edcrfv", "000003"),
+    ],
+)
+def test_student_edit(name, surname, student_id):
+    student = Student(name, surname, student_id)
+    student.name = name+"a"
+    student.surname = surname+"b"
+    student.id = student_id+"1"
+    assert student.name == (name+"a")
+    assert student.surname == (surname+"b")
+    assert student.id == (student_id+"1")
+
+
+
+
 
