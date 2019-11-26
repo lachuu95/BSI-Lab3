b24851ba204d9cb2d7429de5ac6c490b8c278910
Błażej Łach <lachuu95@gmail.com>
2019-11-26 07:16:26 +0100

    dodanie nagłówków testów

--- test/src/test_student.py
+++ test/src/test_student.py
@@ -46,6 +46,35 @@ def test_student_edit(name, surname, student_id):
     assert student.surname == (surname+"b")
     assert student.id == (student_id+"1")
 
+@pytest.mark.parametrize(
+    "name, surname, student_id",
+    [
+        ("awerty", "azerty", "000001"),
+        ("213546", ",./;l=", "000002"),
+        ("Qazwsx", "Edcrfv", "/';.][]"),
+    ],
+)
+def test_student_edit_wrong(name, surname, student_id):
+    student = Student("Qwerty", "Azerty", "000001")
+    with pytest.raises(ValueError):
+        student.name = name
+        student.surname = surname
+        student.id = student_id
+
+def test_student_add_mark():
+
+def test_student_edit_mark():
+
+def test_student_mean():
+
+def test_student_add_meet():
+
+def test_student_set_presence():
+
+def test_student_edit_presence():
+
+
+
 
 
 
